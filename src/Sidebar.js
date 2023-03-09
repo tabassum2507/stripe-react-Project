16d5e75b34c151cd04e52b65dec8c06d4788a41a
tabassum2507 <10tabassum2@gmail.com>
2023-03-09 18:55:33 +0530

    data

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+import { useGlobalContext } from './context'
+import sublinks from './data'
 
 const Sidebar = () => {
+  const {isSidebarOpen, closeSidebar} = useGlobalContext()
   return (
-    <div>Sidebar</div>
+    <div
+      className={`${
+        isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+      }`}
+    >
+      <aside className='sidebar'>
+        <button onClick={closeSidebar} className='close-btn'><FaTimes />
+        </button>
+
+        <div className="sidebar-links">
+          {sublinks.map((item, index) => {
+            const {links, page} = item;
+            return (<article key={index}>
+              <h4>{page}</h4>
+            </article>
+            )
+          })}
+        </div>
+        
+      </aside>
+    </div>
   )
 }
 
